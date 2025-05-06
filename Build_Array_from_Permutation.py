d0f239b50300d945b5c592209ddd97f9ded15b75
SRAVAN KUMAR MALLA <147217486+SravanKuamarMalla999@users.noreply.github.com>
2025-05-06 21:19:23 +0530

    Create Build_Array_from_Permutation.py

---
+++ Build_Array_from_Permutation.py
@@ -0,0 +1,8 @@
+from typing import List  # Needed for List[int] to work
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
